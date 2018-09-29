4ea8101b61e0688aa085eadfed2a7f90a090cd94
J-888 <josemigueltajuelo@gmail.com>
2018-09-29 19:34:17 +0200

    API key security for eval.js

--- custom_functions.js
+++ custom_functions.js
@@ -18,7 +18,10 @@ module.exports = (client) => {
 		text = text
 		.replace(/`/g, "`" + String.fromCharCode(8203))
 		.replace(/@/g, "@" + String.fromCharCode(8203))
-		.replace(client.token, "mfa.VkO_2G4Qv3T--NO--lWetW_tjND--TOKEN--QFTm6YGtzq9PH--4U--tG0");
+		
+		.replace(client.config.token, "");
+		.replace(client.config.fmAPIkey, "");
+		.replace(client.config.geniusAPIkey, "");
 
 		return text;
 	};
