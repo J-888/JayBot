a12c15cfe8111c87f63073a728126376397301b6
J-888 <josemigueltajuelo@gmail.com>
2018-09-29 02:06:12 +0200

    upgraded ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,10 @@
 module.exports = {
-    name: 'ping',
-    description: 'Ping!',
-    cooldown: 5,
-    guildOnly: true,
-    execute(message, args) {
-        message.channel.send('Pong.');
-    },
+	name: 'ping',
+	description: 'Ping!',
+	cooldown: 5,
+	guildOnly: true,
+	async execute(message, args) {
+		const msg = await message.channel.send("Ping? . . .");
+		msg.edit(`Pong!\nBot Latency is ${msg.createdTimestamp - message.createdTimestamp}ms\nAPI Latency is ${Math.round(message.client.ping)}ms`);
+	},
 };
