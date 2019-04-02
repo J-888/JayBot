cd1064967ba06aff4fb98d31e48bf68bb41c892d
J-888 <josemigueltajuelo@gmail.com>
2019-04-02 14:11:56 +0200

    Minor update

--- commands/react.js
+++ commands/react.js
@@ -5,12 +5,16 @@ module.exports = {
     args: true,
     usage: '<emoji1> [emoji2] [emoji3]...',
 	async execute(message, args) {
+    
 		var emoji;
 		for (let i=0; i<args.length; ++i){
 			emoji = message.guild.emojis.find(x => x.name === args[i]);
 			if(emoji != undefined){
 				await message.react(emoji);
 			}
+      else{
+        await message.react(args[i]);
+      }
 		}
 	},
 };
