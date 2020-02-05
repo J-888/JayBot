c8efc4da8aca0b652bc88fcb9592fb639909191c
José Miguel Tajuelo Garrigós <josemigueltajuelo@gmail.com>
2020-02-05 16:48:28 +0100

    Log changes

--- index.js
+++ index.js
@@ -13,6 +13,54 @@ setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
 
+const winston = require('winston');
+const {Loggly} = require('winston-loggly-bulk');
+require('winston-daily-rotate-file');
+
+const consoleTransport = new winston.transports.Console({
+	format: winston.format.combine(
+		winston.format.colorize(),
+		winston.format.timestamp(),
+		winston.format.prettyPrint(),
+		winston.format.splat(),
+		winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`)
+	)
+});
+
+const DRFtransport = new (winston.transports.DailyRotateFile)({
+	filename: './log-%DATE%.log',
+	dirname: 'logs',
+	datePattern: 'YYYY-MM-DD--HH-mm',
+	frequency: '1d',
+	maxSize: '20k',
+	maxFiles: '7d'
+});
+const DRFException = new (winston.transports.DailyRotateFile)({
+	filename: './exc-%DATE%.log',
+	dirname: 'logs',
+	datePattern: 'YYYY-MM-DD--HH-mm',
+	frequency: '1d',
+	maxSize: '20k',
+	maxFiles: '7d'
+});
+DRFtransport.on('rotate', function(oldFilename, newFilename) {
+	console.log('rotate from ' + oldFilename +' to ' + newFilename);
+});
+
+const LogglyTransport = new Loggly({
+    token: "189de478-68cf-4cfc-97a1-9c20e8816d2c",
+    subdomain: "JMTG888",
+    tags: ["Winston-NodeJS"],
+    json: true
+});
+
+var logger = winston.createLogger({
+	exitOnError: false,
+	//format: formatFix,
+	transports: [consoleTransport, DRFtransport, LogglyTransport],
+	exceptionHandlers: [consoleTransport, DRFException, LogglyTransport, new winston.transports.File({ filename: 'exceptions.log', timestamp: true })]
+});
+
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
@@ -104,4 +152,4 @@ client.on('message', message => {
 	}
 });
 
-client.login(client.config.token);
+//client.login(client.config.token);
